fc24a39d6712753c5e56e11da39d0157b5312285
Eduardo <edujimenez621@gmail.com>
2025-04-22 14:40:27 -0600

    Modificacion a fecha.sh

--- comandos/fecha.sh
+++ comandos/fecha.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Formato de fecha: YYYY-MM-DD
+fecha=$(printf '%(%d-%m-%Y)T' -1)
+
+# Formato de hora: HH:MM:SS
+hora=$(printf '%(%H:%M:%S)T' -1)
+
+echo "Fecha actual: $fecha"
+echo "Hora actual:  $hora"
 
