9da56df5a6a203453ff71b25386c0d0fa6818f36
Eduardo <edujimenez621@gmail.com>
2025-04-22 14:18:16 -0600

    Estructura basica de los creditos

---
+++ comandos/creditos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "============================================="
+echo "                 CRÉDITOS"
+echo "============================================="
+echo "Proyecto de terminal personalizada en Bash"
+echo
+echo "Desarrollado por:"
+
+echo "Eduardo Yael Jiménez Sánchez"
+#echo "pon tu nombre porfa ian"
+
+echo
+
+echo "Este proyecto tiene como objetivo crear una terminal personalizada en Bash"
+echo "con comandos básicos de búsqueda, navegación y juegos para aprendizaje."
+echo
+echo "Gracias a Protecsa y todos los instructores."
+echo "============================================="
