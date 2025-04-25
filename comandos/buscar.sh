15bc9d1964077f67a53c351288d50343602c0831
Eduardo <edujimenez621@gmail.com>
2025-04-24 20:15:35 -0600

    Correccion para redirigir errores

--- comandos/buscar.sh
+++ comandos/buscar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pedir al usuario que ingrese el directorio y el archivo
-read -p "Ingrese el directorio donde buscar: " directorio
+read -p "Ingrese el directorio donde buscar (ruta): " directorio
 read -p "Ingrese el nombre del archivo a buscar: " archivo
 
 # Verificar si el directorio existe
@@ -11,12 +11,13 @@ if [ ! -d "$directorio" ]; then
 fi
 
 # Buscar el archivo en el directorio
-busqueda=$(find "$directorio" -type f -name "$archivo")
+busqueda=$(find "$directorio" -name "$archivo" 2>/dev/null)
 
 # Comprobar si el archivo fue encontrado
 if [ -z "$busqueda" ]; then
     echo "ERROR: El archivo '$archivo' no existe en '$directorio'."
     exit 1
 else
-    echo "Archivo '$archivo' encontrado en: $busqueda"
+    echo "Archivo '$archivo' encontrado en:"
+    echo "$busqueda"
 fi
